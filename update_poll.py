1080a3ba77e2b6b8e1ab6e9f665b55312f463aac
Drakomire <Bambolambo0@gmail.com>
2021-06-24 23:31:22 -0400

    script update

--- update_poll.py
+++ update_poll.py
@@ -6,27 +6,19 @@ from git import Repo
 source_lua = Repo("AzurLaneSourceLua")
 perseus = Repo(".")
 
-perseus.git.add("dist")
-perseus.git.commit("-m", "Distribution data update", "--allow-empty")
-perseus.git.add("AzurLaneSourceJson")
-perseus.index.commit("JSON data update")
-perseus.remotes.origin.push()
+while True:
+    #Poll source JSON
+    res = source_lua.git.pull()
+    if (res == "Already up to date."):
+        pass
+    else:
+        #Update everything
+        print("Repo is not up to date. Updating!")
+        os.system("./run.sh")
+        perseus.git.add("dist")
+        perseus.git.commit("-m", "Distribution data update", "--allow-empty")
+        perseus.git.add("AzurLaneSourceJson")
+        perseus.index.commit("JSON data update")
+        perseus.remotes.origin.push()
 
-# while True:
-#     #Poll source JSON
-#     res = source_lua.git.pull()
-#     if (res == "Already up to date."):
-#         pass
-#     else:
-#         #Update everything
-#         print("Repo is not up to date. Updating!")
-#         os.system("./run.sh")
-#         perseus.git.add("dist")
-#         perseus.git.commit("-m", "data update", "--allow-empty")
-#         perseus.git.add("AzurLaneSourceJson")
-#         perseus.index.commit("JSON data update")
-#         perseus.remotes.origin.push("master")
-
-
-
-#     time.sleep(300)
+    time.sleep(300)

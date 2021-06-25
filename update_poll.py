0526eab7b409818dc1b4195cd8f23fdece08bfa8
Drakomire <Bambolambo0@gmail.com>
2021-06-25 07:37:34 +0000

    Fixed some polling problems

--- update_poll.py
+++ update_poll.py
@@ -8,8 +8,9 @@ perseus = Repo(".")
 
 while True:
     #Poll source JSON
-    res = source_lua.git.pull()
+    res = source_lua.git.pull("https://github.com/Dimbreath/AzurLaneData","master")
     if (res == "Already up to date."):
+        print(f"Repo is up to date as of {int(time.time())}")
         pass
     else:
         #Update everything
@@ -19,6 +20,8 @@ while True:
         perseus.git.commit("-m", "Distribution data update", "--allow-empty")
         perseus.git.add("AzurLaneSourceJson")
         perseus.index.commit("JSON data update")
+        print("Changes commit")
         perseus.remotes.origin.push()
+        print("Repo pushed to orgin!")
 
     time.sleep(300)

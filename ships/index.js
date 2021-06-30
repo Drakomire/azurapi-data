0dcbb1a87a9af1ca5b36918a5ee199fd1392f5e0
Drakomire <Bambolambo0@gmail.com>
2021-06-30 03:25:35 -0400

    Added Chibis and changed how skins are stored

--- ships/index.js
+++ ships/index.js
@@ -369,9 +369,15 @@ function readFilesFromLanguage(lang = "EN") {
             //Open the sublist
             let sublist = JSON.parse(fs.readFileSync(path.join(__dirname, "..", "AzurLaneSourceJson", lang, "sharecfg", "ship_skin_template_sublist",`ship_skin_template_${ship_skin_template.indexs[skin_id]}.json`)).toString());
             let skin = sublist[skin_id]
-            skin_portaits.push(`https://raw.githubusercontent.com/Drakomire/perseus-data/master/AzurLaneImages/assets/artresource/atlas/squareicon/${skin.painting}.png`)
+            skin_portaits.push({
+              "thumbnail" : `https://raw.githubusercontent.com/Drakomire/perseus-data/master/AzurLaneImages/assets/artresource/atlas/squareicon/${skin.painting}.png`,
+              "chibi" : `https://raw.githubusercontent.com/Drakomire/perseus-data/master/AzurLaneImages/assets/artresource/atlas/shipmodels/${skin.painting}.png`
+              
+            })
           }
-          compiled[ship.group_type].skin_thumbnails = skin_portaits
+
+
+          compiled[ship.group_type].skins = skin_portaits
         }
 
 

0e53286c29cdc4c13ebc966071189d7ce8b7b360
Drakomire <Bambolambo0@gmail.com>
2021-07-22 18:38:18 -0400

    Master is new production branch

---
+++ ships/skill_parser/parser.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+
+class Parser(ABC):
+
+    @abstractmethod
+    def __init__():
+        pass
+
+    @abstractmethod
+    def wrapper():
+        pass
+
+    @abstractmethod
+    def parse():
+        pass
